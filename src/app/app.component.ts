3424e25f797ea1b7fac0f6ce9f4b4de784f325bb
Antonio Brito <antubrito@gmail.com>
2024-04-07 20:01:10 -0400

    nav bar y home comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenubarComponent } from './menubar/menubar.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MenubarComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
