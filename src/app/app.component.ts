1d617986f9638734a08d49c8d291b831de93edb6
Antonio Brito <antubrito@gmail.com>
2024-04-09 23:13:33 -0400

    add fotos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenubarComponent } from './menubar/menubar.component';
 import { HomeComponent } from './home/home.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
+import { VideosComponent } from './videos/videos.component';
+import { TeamComponent } from './team/team.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { EventoComponent } from './evento/evento.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MenubarComponent,HomeComponent],
+  imports: [RouterOutlet,MenubarComponent,HomeComponent,CarruselComponent,VideosComponent,TeamComponent,
+           WelcomeComponent,EventoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/clases/clases.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clases',
+  standalone: true,
+  imports: [],
+  templateUrl: './clases.component.html',
+  styleUrl: './clases.component.css'
+})
+export class ClasesComponent {
+
+}
