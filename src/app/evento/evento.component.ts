84304948265099c64d81632705c4c59d68b2431c
Antonio Brito <antubrito@gmail.com>
2024-04-16 00:51:56 -0400

    add videos event welcome

--- src/app/evento/evento.component.ts
+++ src/app/evento/evento.component.ts
@@ -1,12 +1,22 @@
+
+// npm install @fortawesome/angular-fontawesome
+
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+
+import { faSearch,faBell,faUser} from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-evento',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule ],
   templateUrl: './evento.component.html',
   styleUrl: './evento.component.css'
 })
 export class EventoComponent {
 
+  faSearch = faSearch;
+  faBell =faBell;
+  faUser = faUser;
 }
