1721c4e3d9749aab202102afcaa54e710cab980d
Antonio Brito <antubrito@gmail.com>
2024-04-25 22:38:42 -0400

    Final website changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,16 @@ import { TeamComponent } from './team/team.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { EventoComponent } from './evento/evento.component';
 import { FooterComponent } from './footer/footer.component';
-
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
 import { SafeUrlPipe } from './safe-url.pipe';
 import { ClasesComponent } from './clases/clases.component';
+import { DividerComponent } from './divider/divider.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,MenubarComponent,HomeComponent,CarruselComponent,VideosComponent,TeamComponent,
-           WelcomeComponent,EventoComponent,FooterComponent,ClasesComponent],
+           WelcomeComponent,EventoComponent,FooterComponent,ClasesComponent,DividerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/evento/evento.component.ts
+++ src/app/evento/evento.component.ts
@@ -2,21 +2,20 @@
 // npm install @fortawesome/angular-fontawesome
 
 import { Component } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
-import { faSearch,faBell,faUser} from '@fortawesome/free-solid-svg-icons';
+
+
+
 
 
 @Component({
   selector: 'app-evento',
   standalone: true,
-  imports: [FontAwesomeModule ],
+  imports: [ ],
   templateUrl: './evento.component.html',
   styleUrl: './evento.component.css'
 })
 export class EventoComponent {
 
-  faSearch = faSearch;
-  faBell =faBell;
-  faUser = faUser;
+ 
 }
