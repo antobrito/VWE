b88022f8ea348054729eedf148c4ebdfdd695b94
Antonio Brito <antubrito@gmail.com>
2024-11-12 23:33:04 -0500

    add evento and photo

---
+++ src/app/event-tab/event-tab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-tab',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-tab.component.html',
+  styleUrl: './event-tab.component.css'
+})
+export class EventTabComponent {
+
+}
